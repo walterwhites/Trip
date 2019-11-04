d12b09719702e43b6bbe87648c6a6844d1c3f870
walterwhites <hopemagie@gmail.com>
2019-11-04 01:39:00 +0100

    little refacto config server

--- config-server-microservice/src/main/java/com/ecommerce/configservermicroservice/ConfigServerMicroserviceApplication.java
+++ config-server-microservice/src/main/java/com/ecommerce/configservermicroservice/ConfigServerMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.configservermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerMicroserviceApplication {
 
 	public static void main(String[] args) {
