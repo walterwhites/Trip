7cdb3762d050b6e71e155baeec742d69386b7db5
walterwhites <hopemagie@gmail.com>
2019-11-04 15:16:51 +0100

    eureka microservice init

--- adventure-microservice/src/main/java/com/ecommerce/adventure/AdventureApplication.java
+++ adventure-microservice/src/main/java/com/ecommerce/adventure/AdventureApplication.java
@@ -2,10 +2,12 @@ package com.ecommerce.adventure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class AdventureApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdventureApplication.class, args);
