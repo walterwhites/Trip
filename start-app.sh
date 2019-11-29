c3ecada90192630a26541e078755765ca58f1ca9
walterwhites <hopemagie@gmail.com>
2019-11-29 11:05:13 +0100

    script start app

---
+++ start-app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sh stop-app.sh &&
+cd config-server-microservice && nohup mvn spring-boot:run &
+sleep 20 && cd eureka-microservice && nohup mvn spring-boot:run &
+sleep 20 && cd zuul-server-microservice && nohup mvn spring-boot:run &
+sleep 40 && cd category-microservice && nohup mvn spring-boot:run &
+sleep 40 && cd adventure-microservice && nohup mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=9090 &
+sleep 40 && cd adventure-microservice && nohup mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=9011 &
+sleep 40 && cd clientui-microservice && nohup mvn spring-boot:run &
---
+++ stop-app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+lsof -ti:9090 | xargs kill &&
+lsof -ti:9091 | xargs kill &&
+lsof -ti:9092 | xargs kill &&
+lsof -ti:9093 | xargs kill &&
+lsof -ti:9094 | xargs kill &&
+lsof -ti:9095 | xargs kill &&
+lsof -ti:9096 | xargs kill &&
+lsof -ti:9011 | xargs kill &&
+rm -rf */nohup.out
